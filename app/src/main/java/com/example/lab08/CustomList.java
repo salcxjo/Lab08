ecb6746569ffc2d135d4e6bff7ddb4185b59b4aa
salcxjo <salar.miri@gmail.com>
2026-03-03 18:13:00 -0700

    Added DeleteCity

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -17,7 +17,8 @@ public class CustomList {
     public boolean hasCity(City city) {
         return cities.contains(city);
     }
+    public void deleteCity(City city) {
+        cities.remove(city);
+    }
 
-    // Will be implemented later using TDD (leave blank for now)
-    // public boolean hasCity(City city) { ... }
 }
